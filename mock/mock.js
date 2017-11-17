0409d0c7748ccd5d81f6e3cc676265f0f729f622
chenjunjun <junjun.chen@zebra-c.com>
2017-11-17 17:27:26 +0800

    修改

--- mock/mock.js
+++ mock/mock.js
@@ -1,39 +1,62 @@
 const Mock = require('mockjs')
+const path = require('path')
+const glob = require('glob')
 const moment = require('moment')
 const _ = require('lodash')
 var Random = Mock.Random
 
-function getSize () {
-  return Math.round(Math.random() * 600) + 100
-}
+const imgPaths = glob.sync(path.join(__dirname, 'static/*.*')).map((name) => {
+  return /\.(bmp|jpg|jpeg|gif|png)$/.test(name) ? `http://localhost:3000/${name.replace(/^.*[\\\/]/, '')}` : ''
+})
+
 module.exports = function () {
   return {
-    imgs: _.times(50, (n) => {
-      var size = getSize() + 'x' + getSize()
-      var src = 'http://192.168.12.74:3000/'
-      var n = Math.ceil(Math.random() * 23)
-      n = n > 9 ? n : '0' + n
-      src += n + '.jpg'
+    auctionLists: _.times(10, (n) => {
       var eDate = new Date(Random.datetime())
       eDate.setYear(new Date().getFullYear())
       return {
-        id: n,
-        info: Random.csentence(10, 30),
-        src: src,// Random.image(size, Random.color())
-        title: Random.csentence(3, 10),
-        price: `$${(Math.random() * 100 + 10).toFixed(2)}`,
-        end_date: moment(eDate).format('YYYY-MM-DD HH:mm:ss')
+        item_id: n,
+        max_price: Random.integer(10, 10e3),
+        owner_name: Random.cname(),
+        visit_num: Random.integer(0, 1000),
+        item_name: Random.csentence(5, 15),
+        item_img_path: JSON.stringify([imgPaths[Random.integer(0, imgPaths.length - 1)]]),
+        endtime: moment(eDate).format('YYYY-MM-DD HH:mm:ss')
       }
     }),
-    comments: _.times(1000, (n) => {
-      var id = Math.floor(n / 20)
+    lists: _.times(10, (n) => {
       return {
-        id: id,
-        nickname: Random.cname(),
-        content: Random.csentence(1, 200),
-        comt_date: Random.date('yyyy-MM-dd HH:mm:ss'),
-        bid: Math.round(Math.random() * 1000 + 100)
+        item_id: n,
+        bid_date: Random.date('yyyy-MM-dd HH:mm:ss'),
+        bid_price: Math.round(Math.random() * 1000 + 100),
+        message: Random.csentence(10, 30),
+        true_name: Random.cname(),
+        username: Random.string('0123456789', 11)
       }
-    })
+    }),
+    noticeLan: [{
+      'id': '14',
+      'notice_content': '恭喜xxx，成功拍卖一个小魔仙！',
+      'is_use': '1',
+      'addtime': '2017-11-08 08:07:36'
+    }],
+    editAuctionLists: _.times(10, (n) => {
+      var eDate = new Date(Random.datetime())
+      eDate.setYear(new Date().getFullYear())
+      return {
+        item_id: n,
+        max_price: Random.integer(10, 10e3),
+        owner_name: Random.cname(),
+        visit_num: Random.integer(0, 1000),
+        item_name: Random.csentence(5, 15),
+        item_img_path: JSON.stringify([imgPaths[Random.integer(0, imgPaths.length - 1)]]),
+        endtime: moment(eDate).format('YYYY-MM-DD HH:mm:ss')
+      }
+    }),
+    visitAdd: {
+      results: true,
+      rows: null,
+      success: true
+    }
   }
 }
