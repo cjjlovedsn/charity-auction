929f080ba49f705e175468b7b6520d499e854b39
chenjunjun <junjun.chen@zebra-c.com>
2017-12-08 17:46:30 +0800

    用户名去掉手机验证规则，改为可以输入任意内容

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -28,7 +28,7 @@
         viewPwd: 'password-not-view',
         rules: {
           username: {
-            reg: /^1(3|4|5|7|8)\d{9}$/,
+            reg: /^.+$/,
             state: ''
           },
           password: {
